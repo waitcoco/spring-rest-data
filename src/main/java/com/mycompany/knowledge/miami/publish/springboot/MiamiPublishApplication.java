df572040df96f25a498c2c6ba9d43b4569f89dba
Kai Yang <kfstorm@dingtalk.com>
2018-06-14 21:00:40 +0800

    Fix bugs

--- src/main/java/com/mycompany/knowledge/miami/publish/springboot/MiamiPublishApplication.java
+++ src/main/java/com/mycompany/knowledge/miami/publish/springboot/MiamiPublishApplication.java
@@ -2,12 +2,9 @@ package com.mycompany.knowledge.miami.publish.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
