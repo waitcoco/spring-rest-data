da665d52bdbc882e73526c539bde732e8b021afa
waitcoco <jin.zha@husky.neu.edu>
2018-06-27 18:23:01 +0800

    first commit

---
+++ src/main/java/com/mycompany/knowledge/miami/publish/model/gongan/IdentityRelation.java
@@ -0,0 +1,13 @@
+package com.mycompany.knowledge.miami.publish.model.gongan;
+
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+public class IdentityRelation {
+    @Data
+    @Entity
+    @Table(name = "IdentityRelation")
+}
---
+++ src/main/java/com/mycompany/knowledge/miami/publish/model/gongan/PhoneRelation.java
@@ -0,0 +1,4 @@
+package com.mycompany.knowledge.miami.publish.model.gongan;
+
+public class PhoneRelation {
+}
